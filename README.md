8ab5f18188fdbd522198c846abc9abe123677be4
Jorisson <jorisson.v@hotmail.com>
2020-05-25 18:11:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-pwcfyy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-pwcfyy)
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Square from ".Square/Square";
+
+function App () {
+  return <Square />;
+}
+
+render(<App />, document.getElementById('root'));
