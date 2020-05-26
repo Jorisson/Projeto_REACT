1e40329144c6bdf6b3830c4c3a430a8593340ce0
Jorisson <jorisson.v@hotmail.com>
2020-05-26 00:42:18 -0300

    Melhoria no index.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import ColorizedSquad from "./Square/ColorizedSquad";
 
 function App () {
   const [color, setColor] = useState("black");
+
   return (
     <div>
+    <p>Escolha um cor para o Personagem!</p>
       <Square color={color} />
+      <p />
       <ColorizedSquad
         title="Vermelho"
         clicked={() => {
@@ -26,7 +29,7 @@ function App () {
           setColor("blue");
         }}
       />
-    </div>
+      </div>
     );
 }
 
